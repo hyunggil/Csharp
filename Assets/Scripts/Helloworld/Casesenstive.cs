56a855ef37874fc1d4106a89c786286459f16262
DESKTOP-5V3FRMH\M <puzeot@gmail.com>
2025-02-26 17:13:09 +0900

    2/26 작업 완료 c# Hello World!! 구

---
+++ Assets/Scripts/Helloworld/Casesenstive.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Casesensitive : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("C#은 대문자와 소문자를 구분합니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Helloworld/Usingdemo.cs
@@ -0,0 +1,36 @@
+//using사용할때
+/*
+using UnityEngine;
+
+public class Usingdemo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
+
+using UnityEngine;
+
+public class Usingdemo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UnityEngine.Debug.Log("hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
