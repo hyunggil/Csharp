b2789c50c0b9fc1b559ff6d92dfcad59e107567a
DESKTOP-5V3FRMH\M <puzeot@gmail.com>
2025-02-28 13:41:15 +0900

    character 문법

---
+++ Assets/Scripts/BoolCharString/CharacterDemo.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CharacterDemo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        char Eng = 'A';
+        char Kor = '한';
+        Debug.Log("영어는 " + Eng + "한글은 " + Kor);
+    }
+
+
+}
