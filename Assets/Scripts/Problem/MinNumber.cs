b61043994b128e0fd80164c4ced68f1cbbc2d629
DESKTOP-5V3FRMH\M <puzeot@gmail.com>
2025-04-09 17:42:29 +0900

    배열의 최소값을 찾는 프로그램

--- Assets/Scripts/Problem/MinNumber.cs
+++ Assets/Scripts/Problem/MinNumber.cs
@@ -7,7 +7,31 @@ namespace Problem
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            //숫자 배열 선언
+            int[] numbers = { -2, -5, -3, -7, -1 };
+            //배열의 최소값을 찾기
+            int minValue = FindMinValues(numbers);
+            //최소값 출력
+            Debug.Log($"배열의 최소값은: {minValue}");
 
+        }
+        //최소값을 찾는 메서드
+        int FindMinValues(int[] numbers)
+        {
+            //첫번째 값을 최소값으로 지정
+            int min = numbers[0];
+            //배열의 각 원소를 하나씩 비교
+            foreach (var num in numbers)
+            {
+                //만약 현재값이 최소값보다 작으면
+                if(num < min)
+                {
+                    //최소값을 num으로 갱신
+                    min = num;
+                }
+            }
+            //최소값을 반환하라
+            return min;
         }
     }
 }
